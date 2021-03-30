1f177418db66c7288c5af9f2198af43cdfdf7d2a
AThomasBring <73478932+AThomasBring@users.noreply.github.com>
2021-03-30 16:17:45 +0100

    assigmentDone

--- Assignment1AntonAndAlexander/Assignment1/Program.cs
+++ Assignment1AntonAndAlexander/Assignment1/Program.cs
@@ -7,6 +7,12 @@ using System.Linq;
 
 namespace Assignment1
 {
+    public class Movie
+    {
+        public int ID;
+        public string Title;
+        public DateTime ReleaseDate;
+    }
     public class Program
     {
         // We make the connection static because we need it in almost every method.
@@ -17,7 +23,7 @@ namespace Assignment1
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
             // Connect to the database.
-            connection = new SqlConnection(@"Data Source=(local)\SQLExpress;Initial Catalog=Assignment1;Integrated Security=SSPI;");
+            connection = new SqlConnection(@"Data Source=LAPTOP-43J0ADP6\SQLEXPRESS01;Initial Catalog=Assignment1;Integrated Security=SSPI;");
             connection.Open();
 
             bool done = false;
@@ -66,11 +72,30 @@ namespace Assignment1
             }
         }
 
+        private static void GetMovies()
+        {
+            // SqlCommand command = new SqlCommand(option, connection);
+            // SqlDataReader reader = command.ExecuteReader();
+            // reader.Close();
+        }
+
         private static void ListMoviesAlphabetically()
         {
             WriteHeading("Movies A-Z");
 
             // TODO: Get movies from database.
+            string insertTitle = "SELECT * FROM Movie order by Title asc";
+            SqlCommand command = new SqlCommand(insertTitle, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string title = Convert.ToString(reader["Title"]);
+                DateTime releaseDate = Convert.ToDateTime(reader["ReleaseDate"]);
+                Console.WriteLine(" - " + title + " (" + releaseDate.Year + ")");
+            }
+            Console.WriteLine();
+            reader.Close();
         }
 
         private static void ListMoviesByReleaseDate()
@@ -78,13 +103,43 @@ namespace Assignment1
             WriteHeading("Movies by release date");
 
             // TODO: Get movies from database.
+            string sortByRelease = "select * from Movie order by ReleaseDate desc";
+            SqlCommand command = new SqlCommand(sortByRelease, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string title = Convert.ToString(reader["Title"]);
+                DateTime releaseDate = Convert.ToDateTime(reader["ReleaseDate"]);
+
+                Console.WriteLine(title + " (" + releaseDate.Year + ")");
+            }
+            Console.WriteLine();
+            reader.Close();
         }
 
         private static void FindMoviesByYear()
         {
             WriteHeading("Find movies by year");
 
+            Console.WriteLine("What year? ");
+            string searchyear = Console.ReadLine();
+
             // TODO: Get movies from database.
+            string searchByYear = "select * from Movie where year(ReleaseDate) = @searchyear";
+            
+            SqlCommand command = new SqlCommand(searchByYear, connection);
+            command.Parameters.AddWithValue("@searchyear", searchyear);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string title = Convert.ToString(reader["Title"]);
+                DateTime releaseDate = Convert.ToDateTime(reader["ReleaseDate"]);
+                Console.WriteLine(title + " (" + releaseDate.Year + ")");
+            }
+            Console.WriteLine();
+            reader.Close();
         }
 
         private static void AddMovie()
@@ -101,13 +156,52 @@ namespace Assignment1
             int month = int.Parse(Console.ReadLine());
             Console.Write("Day (1-31): ");
             int day = int.Parse(Console.ReadLine());
+            DateTime releaseDate = new DateTime(year, month, day);
+
 
             // TODO: Add movie to database.
+            string insertSql = "INSERT INTO Movie (Title, ReleaseDate) VALUES (@Title, @ReleaseDate)";
+            SqlCommand insertCommand = new SqlCommand(insertSql, connection);
+            insertCommand.Parameters.AddWithValue("@Title", title);
+            insertCommand.Parameters.AddWithValue("@ReleaseDate", releaseDate);
+            insertCommand.ExecuteNonQuery();
+            Console.WriteLine();
+
         }
 
         private static void DeleteMovie()
         {
+            List<Movie> movieList = new List<Movie>();
+            
+            string insertTitle = "SELECT * FROM Movie order by Title asc";
+            SqlCommand command = new SqlCommand(insertTitle, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var movie = new Movie();
+                movie.ID = Convert.ToInt32(reader["ID"]);
+                movie.Title = Convert.ToString(reader["Title"]);
+                movie.ReleaseDate = Convert.ToDateTime(reader["ReleaseDate"]);
+                movieList.Add(movie);
+            }
+            reader.Close();
+
+            List<string> options = new List<string>();
+            foreach (var movie in movieList)
+            {
+                options.Add(movie.Title + " ( " + movie.ReleaseDate.Year + " ) ");
+            }
+            int index = ShowMenu("Delete Movie ", options.ToArray());
+            var selectedMovie = movieList[index].ID;
+            
             // TODO: Delete movie from database.
+            string insertSql = "delete from Movie where ID = @ID";
+            SqlCommand insertCommand = new SqlCommand(insertSql, connection);
+            insertCommand.Parameters.AddWithValue("@ID", selectedMovie);
+            insertCommand.ExecuteNonQuery();
+            Console.WriteLine("Movie " + options[selectedMovie] + " deleted!");
+            Console.WriteLine();
         }
 
         // The third parameter is the default option, which allows us to, for example, "remember" where the user was when they return to the main menu.
